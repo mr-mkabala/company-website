f35879544ceea3a84852c8aba4a35a64e9426885
Mateusz <mat.kabala.91@gmail.com>
2022-02-23 16:13:04 +0100

    Add GSAP animations for start, ScrollTo and ScrollTrigger plus add nodemailer functionality to contact form.

--- app.js
+++ app.js
@@ -1,10 +1,66 @@
-const express = require('express');
-const app = express();
+require('dotenv').config();
 
+const express = require('express');
+const nodemailer = require('nodemailer');
+const multiparty = require('multiparty');
+
+const app = express();
 app.use(express.static('public'));
+
+const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 587,
+    auth: {
+      user: process.env.EMAIL,
+      pass: process.env.PASS,
+    }
+});
+
+transporter.verify(function (error, success) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log("Server ready to take messages.");
+    }
+  });
+  
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/200', (req, res) => {
+    res.sendFile(__dirname + '/msg200.html');
+});
+
+app.get('/500', (req, res) => {
+    res.sendFile(__dirname + '/msg500.html');
+});
+
+app.post("/", (req, res) => {
+    let form = new multiparty.Form();
+    let data = {};
+    form.parse(req, function (err, fields) {
+        Object.keys(fields).forEach(function (property) {
+            data[property] = fields[property].toString();
+        });
+  
+        const mail = {
+            from: data.fName,
+            to: process.env.EMAIL,
+            subject: "Freelancing - wiadomość od " + data.fName + " " + data.lName,
+            text: `${data.fName} ${data.lName} <${data.email}> \n${data.message}`
+        };
+  
+        transporter.sendMail(mail, (err, data) => {
+            if (err) {
+                console.log(err);
+                res.status(500).redirect('/500');
+            } else {
+                res.status(200).redirect('/200');
+            }
+        });
+    });
 });
 
 app.listen(process.env.PORT || 3000, () => {
--- public/js/navigation-bar-toggler.js
+++ public/js/navigation-bar-toggler.js
@@ -1,7 +1,7 @@
-let prevScrollpos = window.pageYOffset;
+let prevScrollpos = window.pageYOffset; // ile pikseli do dołu przescrollowana jest strona
 window.onscroll = function() {
-  var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
+  let currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) { // sprawdza czy obecna pozycja scrolla jest mniejsza od początkowej
     document.querySelector('.navigation-bar').style.top = "0";
   } else {
     document.querySelector('.navigation-bar').style.top = "-85px";
